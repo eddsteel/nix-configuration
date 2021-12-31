2b9151e15d45712e8648dd753a2a2236f1393424
Edd Steel <edd@eddsteel.com>
2021-12-31 14:00:06 -0800

    update

--- hosts/draper.nix
+++ hosts/draper.nix
@@ -3,8 +3,6 @@
   user = "edd";
   email = "edd@eddsteel.com";
   homeDir = "/home/edd";
-  face.url = "https://cdn002.tintin.com/public/tintin/img/characters/le-capitaine-haddock/captain-haddock.png";
-  face.sha = "0672magv1193shrmdyjfv1mjmgbaqrhlnsbi9jqk3y5wjx69gfb2";
   src.repos = [
     {"name" = "ledger"; "remote" = "git@eddsteel.com:diane.git"; "stow" = true;}
     {"name" = "df-emacs"; "stow" = true;}
