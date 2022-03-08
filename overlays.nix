d4666cddf374d28186f07748fdc15f6170196c5c
Edd Steel <edward@treasure-data.com>
2022-03-08 11:38:44 -0800

    Fixup linux changes merge and update karabiner

--- overlays.nix
+++ overlays.nix
@@ -12,16 +12,17 @@
       pkgs = super;
     in
       pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
-        my-emacs         = pkgs.emacsMacPort;
+        my-emacs         = pkgs.emacsMacport;
         bitwarden        = pkgs.callPackage pkgs/mac/bitwarden.nix { inherit pkgs; };
         firefox          = pkgs.callPackage pkgs/mac/firefox.nix { inherit pkgs; };
+        intellij-idea-ce = pkgs.callPackage pkgs/mac/intellij.nix { inherit pkgs; };
         istat-menus      = pkgs.callPackage pkgs/mac/istat-menus.nix { inherit pkgs; };
         iterm2           = pkgs.callPackage pkgs/mac/iterm.nix { inherit pkgs; };
+        signal-desktop   = pkgs.callPackage pkgs/mac/signal.nix { inherit pkgs; };
+        soundsource      = pkgs.callPackage pkgs/mac/soundsource.nix { inherit pkgs; };
         spectacle        = pkgs.callPackage pkgs/mac/spectacle.nix { inherit pkgs; };
         wavebox          = pkgs.callPackage pkgs/mac/wavebox.nix { inherit pkgs; };
         xbar             = pkgs.callPackage pkgs/mac/xbar.nix { inherit pkgs; };
-        signal-desktop   = pkgs.callPackage pkgs/mac/signal.nix { inherit pkgs; };
-        intellij-idea-ce = pkgs.callPackage pkgs/mac/intellij.nix { inherit pkgs; };
       }
   )
 ]
