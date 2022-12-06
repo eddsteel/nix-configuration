eca5732703080fbe7ee92650afcf46ad728b4dfe
Edd Steel <edd@eddsteel.com>
2022-12-05 17:43:03 -0800

    Remove hub/circle CI from common apps

--- home/apps.nix
+++ home/apps.nix
@@ -2,8 +2,8 @@
 {
   home.packages = with pkgs; [
     duplicati ripgrep mpv unzip awscli2 aspell aspellDicts.en git-web-link envchain tree
-    bitwarden signal-desktop moreutils exfalso
-    hub-local circleci-cli
+    moreutils exfalso bitwarden
+#    hub-local circleci-cli
   ];
 
   xdg.configFile."zoomus.conf".source = ./files/zoomus.conf;
