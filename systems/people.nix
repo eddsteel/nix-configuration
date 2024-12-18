db19a4dd81cee163b3c520a6a6b020863870441b
Edd Steel <edd@eddsteel.com>
2024-12-18 11:40:28 -0800

    retire gusting

--- systems/people.nix
+++ systems/people.nix
@@ -2,5 +2,5 @@
 rec {
   pubkey = user: host: builtins.readFile (./. + "/${host}/id_rsa.${user}.pub");
   seckey = user: host: builtins.readFile (<nix-config> + "/keys/id_rsa.${user}.${host}");
-  pubkeys = [(pubkey "edd" "ringo") (pubkey "edd" "gusting") (pubkey "edd" "draper")];
+  pubkeys = [(pubkey "edd" "ringo") (pubkey "edd" "draper")];
 }
