a389b5372ef6d12d2a743ee810d829d7cce79a57
Edward Steel <edward@treasure.ai>
2026-08-21 16:30:49 -0700

    fix typo

--- modules/home/layers/workstation.nix
+++ modules/home/layers/workstation.nix
@@ -75,7 +75,7 @@ in with lib; {
       keybind = ctrl+shift+c=copy_to_clipboard
       keybind = ctrl+shift+x=text:\t
       keybind = ctrl+shift+v=paste_from_clipboard
-      'a'keybind = global:cmd+backquote=toggle_quick_terminal
+      keybind = global:cmd+backquote=toggle_quick_terminal
     '';
       
     programs.gradle = {
